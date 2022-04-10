75afb6ceee5afee69ae5af2d1c813d31e8a0bc05
LukasAVB <lukasavb@gmail.com>
2022-04-10 00:51:24 -0400

    style(dashboard): responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,13 @@
 	<div id="dashboard" class="container h-screen overflow-hidden grid grid-cols-12 grid-row-min gap-5">
 		<header class="bg-gray-001 col-span-12 p-5 rounded-default shadow-outer">
 			<div class="w-full flex flex-row flex-nowrap justify-between items-center">
-				<div class="">
-					<h1 class="text-primary text-h4">Decentralized Cloud Storage</h1>
+				<div class="flex flex-row flex-nowrap items-center">
+					<mdicon class="text-primary mr-5" name="cloud" :size="50" />
+					<h1 class="hidden lg:block text-primary text-h4">Decentralized Cloud Storage</h1>
 				</div>
 				<div class="flex flex-row flex-nowrap">
 					<div class="flex flex-row flex-nowrap">
-						<div class="mr-5 text-right">
+						<div class="flex flex-row flex-nowrap items-center mr-5 text-right">
 							<h2 class="text-h6 text-primary">Lorem Ipsum</h2>
 							<span class="hidden text-body-2 leading-none">
 								<span class="inline-block w-2 h-2 bg-secondary rounded-full mr-1"></span> Online
@@ -21,24 +22,30 @@
 			</div>
 		</header>
 		<section class="flex flex-col bg-gray-001 col-span-12 p-5 rounded-default shadow-outer">
+			<input 
+				class="hidden"
+				type="file"
+				ref="fileUpload"
+				@change="onFilePicked"
+			>
 			<header class="flex flex-row flex-nowrap justify-between mb-5">
 				<div>
 				</div>
 				<div>
-					<button>
-						<UiIcon name="plus" :size="50"/>
+					<button @click="$refs.fileUpload.click()">
+						<UiIcon name="upload" :size="50"/>
 					</button>
 				</div>
 			</header>
 			<div class="flex-1w-full h-full bg-white shadow-inner">
 				<div class="w-full h-full content-center">
 					<div class="text-center">
-						<h3 class="text-primary mb-3">You don't have any files yet</h3>
-						<p class="mb-6">
+						<h3 class="text-body-2 md:text-h3 text-primary mb-3">You don't have any files yet</h3>
+						<p class="text-small md:text-body mb-6">
 							Add your first file by dragging it here or<br>
 							use the upload new file button on the bottom
 						</p>
-						<UiButton bg-color="primary" text-color="white">Upload</UiButton>
+						<UiButton bg-color="primary" text-color="white" @click="$refs.fileUpload.click()">Upload</UiButton>
 					</div>
 				</div>
 			</div>
