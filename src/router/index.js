257aeaee2518b30d418c334b9ee87753f652586b
LukasAVB <lukasavb@gmail.com>
2022-04-10 20:50:14 -0400

    fix(router): root url

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import store from '../store/index.js';
 import Home from '@/views/Home.vue'
+import { processExpression } from '@vue/compiler-core';
 // import Auth from '@/views/Auth.vue'
 
 const routes = [
 	{
-		path: '/',
+		path: process.env.BASE_URL,
 		name: 'Home',
 		component: Home,
 		meta: {
@@ -14,7 +15,7 @@ const routes = [
 	},
 	{
 		path: '/:catchAll(.*)',
-		redirect: '/',
+		redirect: process.env.BASE_URL,
 	}
 ]
 
